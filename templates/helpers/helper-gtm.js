ef7a06f053158685c04aad24f4d7b77ecacb1efb
Marie Hogebrandt <marie.hogebrandt@pineberry.com>
2015-04-02 11:58:48 +0200

    Break out helpers as submodule

--- templates/helpers/helper-gtm.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-module.exports.gtm = function () {
-    return this.options.gtm;
-};
--- templates/helpers/helper-now.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-module.exports.now = function () {
-    var moment = require('moment');
-    return moment().format('MMMM Do YYYY');
-};
