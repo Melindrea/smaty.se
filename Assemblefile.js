f199ff6337d2a97573914fc9c79249d078618537
Marie Hogebrandt <marie.hogebrandt@pineberry.com>
2015-04-30 06:54:20 +0200

    Updating contents

--- Assemblefile.js
+++ Assemblefile.js
@@ -42,7 +42,6 @@ helpers = helperFiles.reduce(function (acc, fp) {
 // Load system - These three should be broken out and put in assemble-system
 assemble.layouts(system.root + '/' + system.layouts + '/**.hbs');
 assemble.helpers(helpers);
-assemble.helpers(require('handlebars-helpers'));
 assemble.helper('moment', require('helper-moment'));
 assemble.partials(system.root + '/' + system.partials + '/**/**.hbs');
 assemble.data(system.root + '/data/**/*.{yaml,json}');
