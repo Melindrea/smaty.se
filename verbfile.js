c01cb54ea5c9b68466d3320b92e43dcad32de44c
Marie Hogebrandt <marie@pineberry.com>
2015-05-07 12:52:30 +0200

    Update so that nano badge works

--- verbfile.js
+++ verbfile.js
@@ -5,8 +5,8 @@ var verb = require('verb');
 verb.disable('debugEngine');
 
 verb.task('readme', function () {
-  verb.src('.verb.md')
-    .pipe(verb.dest('.'))
+    verb.src('.verb.md')
+        .pipe(verb.dest('.'));
 });
 
 verb.task('default', ['readme']);
