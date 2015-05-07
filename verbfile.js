5d668e7b203c8d1694a635da6e93acbb357b08f0
Marie Hogebrandt <marie@pineberry.com>
2015-05-07 10:54:52 +0200

    Implement verb with a basic readme-template

---
+++ verbfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var verb = require('verb');
+
+verb.disable('debugEngine');
+
+verb.task('readme', function () {
+  verb.src('.verb.md')
+    .pipe(verb.dest('.'))
+});
+
+verb.task('default', ['readme']);
